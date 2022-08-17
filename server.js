a6e4e935d4b781e03a3e14ece36a0bc083a64952
Itismywinningseason <itismywinningseason@gmail.com>
2022-08-17 18:16:53 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const server = express();
 const cors = require("cors");
-server.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+server.use(cors({ credentials: true, origin: "http://localhost:300" , "https://season-blog-frontend.herokuapp.com" }));
 const bodyParser = require("body-parser");
 server.use(bodyParser.json());
 const bcrypt = require("bcrypt");
